2c4e576249578dba0ff651d20263e71bec3f31e4
it24102407 <IT24102407@my.sliit.lk>
2024-08-28 12:13:57 +0530

    IT24102407Lab6Q2A.java

---
+++ Q2/IT24102407Lab6Q2A.java
@@ -0,0 +1,15 @@
+
+public class IT24102407Lab6Q2A
+{
+ public static void main(String[]args)
+   {
+   	
+	int num =1;
+	
+	while(num <= 10)
+    {
+		System.out.print(num +" ");
+		num= num + 1;
+	}		
+	}
+	} 
